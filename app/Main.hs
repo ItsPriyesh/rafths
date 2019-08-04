af24f2d1a0eda781d881248a6ac90d6dd7be1f41
Priyesh Patel <p99patel@uwaterloo.ca>
2019-08-03 20:46:50 -0400

    setup thrift client for comm between cluster nodes

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
 module Main where
 
 import RaftNode
-import qualified Thrift.Server as Th
+import qualified Thrift.Server as T
 import qualified ClusterNode
+import qualified ClusterNode_Client as Client
+import qualified Rafths_Types as T
 
 main :: IO ()
-main =  do
-  handler <- RaftNode.newNodeHandler
-  print "Starting the server..."
-  _ <- Th.runBasicServer handler ClusterNode.process 9090
-  print "closing down server"
+main = do
+  handler <- RaftNode.newNodeHandler [("localhost", 8080)]
+  print "Starting server..."
+  client <- RaftNode.newThriftClient "localhost" 8080
+  response <- Client.requestVote client T.VoteRequest { T.voteRequest_candidateId = 0 , T.voteRequest_term = 0, T.voteRequest_lastLogTerm = 0, T.voteRequest_lastLogIndex = 0 }
+  print response
+  -- _ <- T.runBasicServer handler ClusterNode.process 8080 -- todo runThreadedServer
+  print "Server terminated!"
--- src/RaftNode.hs
+++ src/RaftNode.hs
@@ -2,26 +2,62 @@ module RaftNode where
 
 import Control.Concurrent.MVar
 
+import Thrift
+import Thrift.Protocol
+import Thrift.Protocol.Binary
+import Thrift.Transport
+import Thrift.Transport.Handle
+import Thrift.Server
+import Network
+
+import System.IO
+
 import ClusterNode_Iface
+import qualified ClusterNode_Client as Client
 import qualified Rafths_Types as T
 
 instance ClusterNode_Iface NodeHandler where
   requestVote = handleRequestVote
   appendEntries = handleAppendEntries
 
-data LogEntry = LogEntry { key :: String , value :: String } deriving (Show)
+data Role = Follower State | Candidate State | Leader State
 
-newtype NodeHandler = NodeHandler { getLog :: MVar [LogEntry] }
+-- fold over [LogEntry] to get current state of kv store
+data State = State {
+  currentTerm :: Int,
+  votedFor :: Maybe Int,
+  log :: [LogEntry],
+  commitIndex :: Int,
+  lastApplied :: Int
+}
 
-newNodeHandler :: IO NodeHandler
-newNodeHandler = do
-  log <- newMVar mempty
-  pure $ NodeHandler log
+emptyState :: State
+emptyState = State 0 Nothing [] 0 0
 
+data LogEntry = LogEntry { key :: String , value :: String }
+
+data ServerState = ServerState { role :: Role, peers :: [(String, Int)] }
+
+newtype NodeHandler = NodeHandler { serverState :: MVar ServerState }
+
+newNodeHandler :: [(String, Int)] -> IO NodeHandler
+newNodeHandler peers = do
+  newServerSate <- newMVar (ServerState (Follower emptyState) peers)
+  pure $ NodeHandler newServerSate
+
+-- RPC Handlers
 handleRequestVote :: NodeHandler -> T.VoteRequest -> IO T.VoteResponse
 handleRequestVote handler request = do
+  print "request vote called!"
   pure T.VoteResponse { T.voteResponse_term = 1, T.voteResponse_granted = True }
 
 handleAppendEntries :: NodeHandler -> T.AppendRequest -> IO T.AppendResponse
 handleAppendEntries handler request = do
   pure T.AppendResponse { T.appendResponse_term = 1, T.appendResponse_success = True }
+
+-- Creates a client RPC stub referencing another node in the cluster
+newThriftClient :: String -> Integer -> IO (BinaryProtocol Handle, BinaryProtocol Handle)
+newThriftClient host port = do
+  transport <- hOpen (host, (PortNumber . fromInteger $ port))
+  let proto = BinaryProtocol transport
+  pure (proto, proto)
