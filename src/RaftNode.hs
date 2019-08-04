7298983d27fca86a7984eea1f232b86d778ba265
Priyesh Patel <p99patel@uwaterloo.ca>
2019-08-03 23:06:26 -0400

    implement requestVote handler

--- src/RaftNode.hs
+++ src/RaftNode.hs
@@ -9,6 +9,8 @@ import Thrift.Transport
 import Thrift.Transport.Handle
 import Thrift.Server
 import Network
+import Data.Maybe
+import Data.Int
 
 import System.IO
 
@@ -17,18 +19,23 @@ import qualified ClusterNode_Client as Client
 import qualified Rafths_Types as T
 
 instance ClusterNode_Iface NodeHandler where
-  requestVote = handleRequestVote
-  appendEntries = handleAppendEntries
+  requestVote = requestVoteHandler
+  appendEntries = appendEntriesHandler
 
-data Role = Follower State | Candidate State | Leader State
+data Role = Follower State | Candidate State | Leader State [Int] [Int]
+
+getRaftState :: Role -> State
+getRaftState (Follower s) = s
+getRaftState (Candidate s) = s
+getRaftState (Leader s _ _) = s
 
 -- fold over [LogEntry] to get current state of kv store
 data State = State {
-  currentTerm :: Int,
-  votedFor :: Maybe Int,
-  log :: [LogEntry],
-  commitIndex :: Int,
-  lastApplied :: Int
+  currentTerm :: Int64,
+  votedFor :: Maybe Int32,
+  logEntries :: [LogEntry],
+  commitIndex :: Int32,
+  lastApplied :: Int32
 }
 
 emptyState :: State
@@ -42,17 +49,32 @@ newtype NodeHandler = NodeHandler { serverState :: MVar ServerState }
 
 newNodeHandler :: [(String, Int)] -> IO NodeHandler
 newNodeHandler peers = do
-  newServerSate <- newMVar (ServerState (Follower emptyState) peers)
-  pure $ NodeHandler newServerSate
+  state <- newMVar (ServerState (Follower emptyState) peers)
+  pure $ NodeHandler state
+
+getServerState :: NodeHandler -> IO ServerState
+getServerState = readMVar . serverState
 
 -- RPC Handlers
-handleRequestVote :: NodeHandler -> T.VoteRequest -> IO T.VoteResponse
-handleRequestVote handler request = do
+requestVoteHandler :: NodeHandler -> T.VoteRequest -> IO T.VoteResponse
+requestVoteHandler handler request = do
   print "request vote called!"
-  pure T.VoteResponse { T.voteResponse_term = 1, T.voteResponse_granted = True }
+  serverState <- getServerState handler
+  let state = getRaftState $ role $ serverState
+  pure $ newVoteResponse (currentTerm state) (shouldGrant state request)
 
-handleAppendEntries :: NodeHandler -> T.AppendRequest -> IO T.AppendResponse
-handleAppendEntries handler request = do
+newVoteResponse :: Int64 -> Bool -> T.VoteResponse
+newVoteResponse term grant = T.VoteResponse { T.voteResponse_term = term, T.voteResponse_granted = grant }
+
+shouldGrant :: State -> T.VoteRequest -> Bool
+shouldGrant state req = if currentTerm state > T.voteRequest_term req then False else grant
+  where
+    voted = maybe True (== T.voteRequest_candidateId req) (votedFor state)
+    upToDate = (length $ logEntries $ state) - 1 <= (fromIntegral $ T.voteRequest_lastLogIndex req)
+    grant = voted && upToDate
+
+appendEntriesHandler :: NodeHandler -> T.AppendRequest -> IO T.AppendResponse
+appendEntriesHandler handler request = do
   pure T.AppendResponse { T.appendResponse_term = 1, T.appendResponse_success = True }
 
 -- Creates a client RPC stub referencing another node in the cluster
